33b891f68a3f79ecaf4d8d39da3f3221210d0019
DyBiT <102919251+DyBiT@users.noreply.github.com>
2022-06-04 20:28:27 +0200

    Cambios estéticos

--- app/src/androidTest/java/com/example/CompraVenta/Sellba/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/CompraVenta/Sellba/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.valarmorghulis.firebaseauth", appContext.getPackageName());
+        assertEquals("com.example.CompraVenta.Sellba", appContext.getPackageName());
     }
 }
