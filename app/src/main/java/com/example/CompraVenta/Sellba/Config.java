846a8753b861fa11bb33a774538cf626b29bd451
DyBiT <102919251+DyBiT@users.noreply.github.com>
2022-06-09 17:41:35 +0200

    Modificaciones estéticas en perfil versión 1.4 09/06/2022

--- app/src/main/java/com/example/CompraVenta/Sellba/Config.java
+++ app/src/main/java/com/example/CompraVenta/Sellba/Config.java
@@ -4,5 +4,5 @@ package com.example.CompraVenta.Sellba;
 /*CLASE PUBLICA GLOBAL CONFIG PARA PASARLE LOS PARAMETROS DEL LOGIN DE GMAIL EN SendMail*/
 public class Config {
     public static final String EMAIL = "sellbacompany@gmail.com";
-    public static final String PASSWORD = "sellba123";
+    public static final String PASSWORD = "hlazadiivhfrslcy";
 }
